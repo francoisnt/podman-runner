ddfdae830fc20721782fb77b0d6d796fb8d3e144
François Naggar-Tremblay <francois.n.tremblay@gmail.com>
2025-11-16 14:07:07 -0500

    100% helpers coversge in unittests

---
+++ tests/unit/test_helpers.py
@@ -0,0 +1,23 @@
+# tests/unit/test_helpers_mocked.py
+from __future__ import annotations
+
+from unittest.mock import patch
+
+import pytest
+
+
+def test_get_podman_exe_found() -> None:
+    """Test when podman is in PATH."""
+    with patch("shutil.which", return_value="/usr/bin/podman"):
+        from podman_py.helpers import get_podman_exe
+
+        assert get_podman_exe() == "/usr/bin/podman"
+
+
+def test_get_podman_exe_not_found() -> None:
+    """Test when podman is NOT in PATH."""
+    with patch("shutil.which", return_value=None):
+        from podman_py.helpers import get_podman_exe
+
+        with pytest.raises(RuntimeError, match="podman not found in PATH"):
+            get_podman_exe()
